436dc6a6eabf612f3ceccaee7e46a8716ecb0c5c
Katelinlis <katelinlis1999@gmail.com>
2021-11-13 00:25:41 +0300

    FIX FIX FIX

--- store/index.js
+++ store/index.js
@@ -27,7 +27,7 @@ export const actions = {
     if (token && namePage === 'login') {
       redirect('/news')
     }
-    if (!token && namePage !== 'login') {
+    if (token && namePage !== 'login') {
       let headers = {}
 
       if (token !== '') {
