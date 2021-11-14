06d82544428b8c81bc81252c94188d41d5cb4b5e
Katelinlis <katelinlis1999@gmail.com>
2021-11-15 00:20:44 +0300

    settings

---
+++ pages/settings.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div style="width: 300px">
+      <label> Имя </label>
+      <input type="text">
+      <br>
+      <label> Фамилия </label>
+      <input type="text">
+      <br>
+      <label> Возраст </label>
+      <input type="number">
+      <br>
+      <label> Страна </label>
+      <input type="text">
+      <br>
+      <label> Город </label>
+      <input type="text">
+      <br>
+      <label> password </label>
+      <input>
+      <br>
+      <button value="save">
+        save
+      </button>
+    </div>
+  </div>
+</template>
+<style scoped>
+input {
+  margin-left: 10px;
+}
+</style>
