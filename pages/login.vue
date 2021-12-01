91c4aeac908202aaf623a5b2560bc2ab56aaf77b
Katelinlis <katelinlis1999@gmail.com>
2021-12-01 23:41:31 +0300

    new auth

--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,83 @@
+<!--
+    Сверстал Михаил Лебединец lebedinets.ru
+    Собрала и подключила к проекту Кейтлин Лис
+-->
 <template>
   <div class="content">
-    <div style="margin: 0 auto;text-align:center">
-      <label for="InputUsername">{{ $t('UserName') }}</label>
-      <input id="InputUsername" v-model="login">
+    <div class="login-container">
+      <div class="login-header">
+        <div class="login-header-text">
+          <span>{{ $t('Login') }}</span>
+        </div>
 
-      <label for="InputPassword">{{ $t('UserPassword') }}</label>
-      <input id="InputPassword" v-model="password" type="password">
-      <br>
-      <label for="SavePassword">{{ $t('SavePassword') }}?</label>
-      <input id="SavePassword" v-model="savePassword" type="checkbox">
+        <div class="login-header-logo">
+          <img src="/img/login/login-header-logo.svg" alt="Логотип OnlyOne">
+        </div>
 
-      <div id="captcha" style="text-align: -webkit-center">
-        <recaptcha />
+        <div class="login-header-line" />
       </div>
-      <div style="margin-top: 15px;">
-        <input type="button" title="Авторизация или регистрация нового аккаунта" :value="$t('Login')" @click="Auth">
+
+      <div class="login-form">
+        <div class="login-form-text">
+          <span>{{ $t('UserName') }}</span>
+        </div>
+        <div class="login-form-input-container">
+          <div class="login-form-input-icon">
+            <img src="/img/login/login-form-input-icon-login.svg">
+          </div>
+
+          <div class="login-form-inputbox">
+            <input v-model="login" type="text" placeholder="Введите Ваш логин">
+          </div>
+        </div>
+
+        <div class="login-form-text">
+          <span>{{ $t('UserPassword') }}</span>
+        </div>
+        <div class="login-form-input-container">
+          <div class="login-form-input-icon">
+            <img src="/img/login/login-form-input-icon-password.svg">
+          </div>
+
+          <div class="login-form-inputbox">
+            <input v-model="password" type="password" placeholder="Введите Ваш пароль">
+          </div>
+
+          <div v-if="false" class="login-form-input-show">
+            <img src="/img/login/login-form-input-show.svg">
+          </div>
+        </div>
+
+        <div style="text-align:center;">
+          <label for="SavePassword">{{ $t('SavePassword') }}?</label>
+          <input id="SavePassword" v-model="savePassword" type="checkbox">
+        </div>
+
+        <div id="captcha" style="text-align: -webkit-center">
+          <recaptcha class="test" />
+        </div>
+
+        <div class="login-form-button">
+          <button title="Войти или создать новый аккаунт" @click="Auth">
+            {{ $t('Login') }}
+          </button>
+        </div>
+      </div>
+
+      <div class="login-footer">
+        <p>При нажатии Войти происходит проверка существования аккаунта, если аккаунт не найден, то вы успешно зарегистрируете новый аккаунт</p>
+      </div>
+
+      <div v-if="false" class="login-footer">
+        <div class="login-footer-string">
+          <span>У Вас нет аккаунта?</span>
+          <a href="#">Регистрация</a>
+        </div>
+
+        <div class="login-footer-string">
+          <span>Вы забыли пароль?</span>
+          <a href="#"><nobr>Восстановить пароль</nobr></a>
+        </div>
       </div>
     </div>
   </div>
@@ -79,31 +142,196 @@ export default Vue.extend({
 })
 </script>
 <style scoped>
-label[for="InputUsername"],label[for="InputPassword"]{
-  display: block;
-}
-input[type="button"]{
-  text-decoration: none;
-  display: inline-block;
-  width: 140px;
-  height: 45px;
-  line-height: 45px;
-  border-radius: 45px;
-  margin: 10px 20px;
-  font-family: 'Montserrat', sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Roboto:wght@500;700&display=swap');
 
-  text-transform: uppercase;
-  text-align: center;
-
-  color: #524f4e;
-  background: white;
-  box-shadow: 0 8px 15px rgba(0, 0, 0, .1);
-  transition: .3s;
+body {
+    background: #E5E5E5;
 }
-input[type="button"]:hover{
-  background: #2EE59D;
-  box-shadow: 0 15px 20px rgba(46, 229, 157, .4);
-  color: white;
-  transform: translateY(-7px);
+
+.test {
+height: 123px;
+}
+
+.login-container {
+    width: 578px;
+    height: 676px;
+
+    /* center */
+    margin-left: auto;
+    margin-right: auto;
+
+    margin-top: 0px;
+
+    background: #FFFFFF;
+
+    border-radius: 14px;
+}
+
+.login-header-text {
+    margin-left: 78px;
+    padding-top: 51px;
+
+    font-size: 30px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+}
+.login-header-logo {
+    position: relative;
+    float: right;
+
+    margin-right: 74.39px;
+    top: -37px;
+}
+.login-header-line {
+    width: 427px;
+    height: 1px;
+
+    /* center */
+    margin-left: auto;
+    margin-right: auto;
+
+    margin-top: 29px;
+
+    background: #D7E2F2;
+}
+
+.login-form-text {
+    margin-left: 78px;
+    margin-top: 29px;
+
+    font-family: "Roboto", sans-serif;
+
+    color: #606580;
+}
+.login-form-input-container {
+    width: 426px;
+    height: 64px;
+
+    /* center */
+    margin-left: auto;
+    margin-right: auto;
+
+    margin-top: 14px;
+
+    background: #F5F8FD;
+
+    border-radius: 6px;
+}
+
+.login-form-input-icon {
+    margin-left: 20.63px;
+    padding-top: 18.56px;
+}
+
+.login-form-inputbox {
+    position: relative;
+
+    margin-left: 64px;
+    top: -45.5px;
+}
+.login-form-inputbox > input {
+    height: 58px;
+    width: 290px;
+
+    border: none;
+    background: none;
+
+    font-size: 14px;
+    color: #8C99B2;
+
+    outline: none;
+}
+
+.login-form-input-show {
+    position: relative;
+    float: right;
+
+    margin-right: 27px;
+    top: -82px;
+}
+.login-form-input-show:hover {
+    cursor: pointer;
+}
+
+.login-form-button {
+    margin-left: 76px;
+}
+.login-form-button > button {
+    width: 426px;
+    height: 64px;
+
+    background: #0F62FE;
+    border: none;
+
+    color: #FFFFFF;
+    font-family: "Inter", sans-serif;
+    font-size: 16px;
+
+    border-radius: 6px;
+
+    transition: 0.25s;
+}
+.login-form-button > button:hover {
+    cursor: pointer;
+    background: #0d57e0;
+}
+
+.login-footer {
+    text-align: center;
+    margin-top: 37px;
+}
+.login-footer-string {
+    font-family: "Inter", sans-serif;
+    margin-bottom: 14px;
+}
+.login-footer-string > span {
+    color: #696F79;
+    margin-right: 6px;
+}
+.login-footer-string > a{
+    color: #0F62FE;
+    text-decoration: none;
+}
+
+@media screen and (max-width: 578px) {
+    .login-container {
+        width: 95%;
+
+    }
+
+    .login-header-text {
+        margin-left: 38px;
+        padding-top: 31px;
+    }
+    .login-header-logo {
+        margin-right: 38px;
+    }
+    .login-header-line {
+        width: auto;
+
+        margin-left: 38px;
+        margin-right: 38px;
+    }
+
+    .login-form-text {
+        margin-left: 38px;
+    }
+    .login-form-input-container {
+        width: auto;
+
+        margin-left: 38px;
+        margin-right: 38px;
+    }
+    .login-form-inputbox > input {
+        width: 65%;
+    }
+
+    .login-form-button {
+        margin-left: 38px;
+        margin-right: 38px;
+    }
+    .login-form-button > button {
+        width: 100%;
+    }
 }
 </style>
