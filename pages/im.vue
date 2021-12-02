42b77fb9dcf9197c4abbb41da85ae73e3d79740c
Katelinlis <katelinlis1999@gmail.com>
2021-12-03 02:54:39 +0300

    upd

--- pages/im.vue
+++ pages/im.vue
@@ -1,30 +1,41 @@
 <template>
   <div>
-    <div style="height:450px;width:100%">
+    <div style="height:450px;width:90%;overflow:hidden;text-align:center">
       <div v-for="(msg,index) in messages" :key="index">
         <div style="display:block;">
-          <div v-if="$store.me && msg.userid===$store.me.id" style="text-align:left">
-            author {{ msg.text }}
+          <div v-if="me && msg.userid==parseInt(me.id)" style="text-align:left">
+            {{ msg.username }}
+            <br>
+            {{ msg.text }}
           </div>
-          <div v-if="msg.me!==1" style="text-align:right">
-            user {{ msg.text }}
+          <div v-if="me && msg.userid!==parseInt(me.id)" style="text-align:right">
+            {{ msg.username }}
+            <br>
+            {{ msg.text }}
           </div>
         </div>
       </div>
       <div style="position: absolute;bottom: 0;width:100%">
-        <input v-model="message" style="height:40px;border: 2px solid gray;width:88%" type="text">
+        <input v-model="message" style="height:40px;border: 2px solid gray;width:75%" type="text">
         <input style="height:40px;border: 2px solid gray;" type="button" value="Send" @click="send">
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data () {
     return {
       messages: [],
       message: ''
     }
+  },
+  computed: {
+    ...mapState({
+      me: state => state.me
+    })
   },
   created () {
     this.$api.get(`/message/get/${this.$route.query.im}`).then((data) => {
