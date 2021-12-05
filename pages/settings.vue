c20f56b5be57868cc525871856ff5cfd005c52bf
Katelinlis <katelinlis1999@gmail.com>
2021-12-06 01:28:01 +0300

    Доработка страницы настроек

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="settings-container">
-    <div class="settings-menu-container">
-      <div class="settings-menu-item">
-        <img src="/img/login/login-form-input-icon-login.svg" @click="()=>Select(0)">
-        <span @click="()=>Select(0)">Общая<br>информация</span>
-      </div>
-      <div class="settings-menu-item">
-        <img src="/img/login/login-form-input-icon-password.svg" @click="()=>Select(1)">
-        <span @click="()=>Select(1)">Безопасность</span>
-      </div>
-      <div class="settings-menu-item">
-        <img src="/img/login/login-form-input-show.svg" @click="()=>Select(2)">
-        <span @click="()=>Select(2)">Приватность</span>
-      </div>
-      <div class="settings-menu-item">
-        <img src="/img/menu/news.svg" @click="()=>Select(3)">
-        <span @click="()=>Select(3)">Удалить<br>аккаунт</span>
-      </div>
-    </div>
     <div class="settings-edit-container">
-      <div class="settings-edit-password" v-if="element===1">
+      <div v-if="element===1" class="settings-edit-password">
         <h3>Смена пароля</h3><br>
         <span>Прошлый пароль</span><br>
         <input type="password" placeholder="Введите прошлый пароль">
@@ -31,10 +13,34 @@
         <br><br>
         <button>Сохранить</button>
       </div>
+      <div v-if="element===2" class="settings-edit-password">
+        <h3>Контакты, ссылки и привязки</h3><br>
+        <span>Личный сайт</span><br>
+        <input type="password" placeholder="ваш личный сайт">
+        <br><br>
+        <span>Email для связи</span><br>
+        <input type="password" placeholder="введите Email">
+        <br><br>
+        <h4>Привязка социальных сетей</h4>
+        <br>
+        Привязка VK
+        <br>
+        Привязка Facebook
+        <br>
+        Привязка Discord
+        <br>
+        Привязка Telegram
+        <br>
+        Привязка Shikimori
+        <br>
+        <button>Сохранить</button>
+      </div>
 
-      <div class="settings-edit-user" v-else-if="element===0">
+      <div v-else-if="element===0" class="settings-edit-user">
+        <h3>Общая информация</h3><br>
         <span>Дата рождения</span><br>
         <input type="date"><br>
+        <br>
         <span>Ваш пол</span><br>
         <select>
           <option>Женский</option>
@@ -42,7 +48,39 @@
           <option>Другой</option>
         </select>
         <br><br>
+        <span>Местоположение</span><br>
+        <input placeholder="Страна"><input style="margin-left:10px" placeholder="Населенный пункт">
+        <br><br>
+        <span>Краткое описание</span><br>
+        <textarea placeholder="Расскажите о себе" style="width:350px;height:100px;background-color:#f5f8fd" />
+        <br>
         <button>Сохранить</button>
+      </div>
+    </div>
+    <div class="settings-menu-container">
+      <div class="settings-menu-item" @click="()=>Select(0)">
+        <img src="/img/login/login-form-input-icon-login.svg">
+        <span>Общая<br>информация</span>
+      </div>
+      <div class="settings-menu-item" @click="()=>Select(1)">
+        <img src="/img/login/login-form-input-icon-password.svg">
+        <span @click="()=>Select(1)">Безопасность</span>
+      </div>
+      <div class="settings-menu-item" @click="()=>Select(2)">
+        <img src="/img/login/login-form-input-show.svg">
+        <span>Контакты, привязки и ссылки</span>
+      </div>
+      <div class="settings-menu-item" @click="()=>Select(3)">
+        <img src="/img/login/login-form-input-show.svg">
+        <span>Заблокированные и контроль запретов</span>
+      </div>
+      <div class="settings-menu-item" @click="()=>Select(4)">
+        <img src="/img/login/login-form-input-show.svg">
+        <span>Приватность</span>
+      </div>
+      <div class="settings-menu-item">
+        <img src="/img/menu/news.svg">
+        <span>Удалить<br>аккаунт</span>
       </div>
     </div>
   </div>
@@ -63,7 +101,7 @@ export default {
 </script>
 <style scoped>
 .settings-menu-container {
-  width: 218px;
+  width: 250px;
   background: #FFFFFF;
   border-radius: 8px;
   margin-left: 8px;
@@ -92,6 +130,7 @@ export default {
   padding-left: 18px;
   padding-right: 18px;
   padding-bottom: 38.5px;
+  text-align: center;
 }
 .settings-edit-password > input {
   height: 32px;
