401c4c68ff0d74c83b8789c9d157a55853ceee54
Katelinlis <katelinlis1999@gmail.com>
2021-12-12 19:51:34 +0300

    new graphql img

--- pages/login.vue
+++ pages/login.vue
@@ -100,15 +100,15 @@ export default Vue.extend({
   },
   head () {
     return {
-      title: 'Авторизация в социальной сети - Only One',
+      title: 'Авторизация в Only One | Свободная социальная сеть',
       meta: [
         {
           hid: 'description',
           name: 'description',
           content: 'Добро пожаловать в свободную социальную сеть Only one, тебя тут ждет новый огромный мир, а так же ты сможешь создавать свои миры, нас уже больше 500 человек'
         },
-        { hid: 'og:title', name: 'og:title', content: 'Only One - Свободная социальная сеть' },
-        { hid: 'og:image', name: 'og:image', content: 'http://only-one.su/img/OnlyOneSocial.png' }
+        { hid: 'og:title', name: 'og:title', content: 'Авторизация в Only One | Свободная социальная сеть' },
+        { hid: 'og:image', name: 'og:image', content: 'http://only-one.su/img/OpenGraphLogo.png' }
       ]
     }
   },
