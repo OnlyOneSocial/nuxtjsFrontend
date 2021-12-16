26acd2468cff191ca975baa78b28eeaa9a293e9b
Katelinlis <katelinlis1999@gmail.com>
2021-12-17 00:48:02 +0300

    New post test

--- test/NewPostTest.spec.js
+++ test/NewPostTest.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import NewPost from '@/components/News/newPost.vue'
 
 describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+    const wrapper = mount(NewPost)
     expect(wrapper.vm).toBeTruthy()
   })
 })
