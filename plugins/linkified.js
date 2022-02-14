5538fc3aff3157b91b04d1a4306816a6ec075ba4
Katelinlis <katelinlis1999@gmail.com>
2022-02-14 18:04:25 +0300

    fix reactive

--- plugins/linkified.js
+++ plugins/linkified.js
@@ -12,42 +12,44 @@ function escapeAttr (href) {
   return href.replace(/"/g, '&quot;')
 }
 
-function install (el, binding) {
+const object = {
+  bind: (el, binding) => {
   // el.innerHTML = linkifyStr(el.innerHTML, binding.value)
 
-  const opts = new Options(binding.value)
+    const opts = new Options(binding.value)
 
-  const tokens = tokenize(el.innerHTML)
-  const result = []
+    const tokens = tokenize(el.innerHTML)
+    const result = []
 
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i]
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i]
 
-    if (token.t === 'nl' && opts.nl2br) {
-      result.push('<br>\n')
-      continue
-    } else if (!token.isLink || !opts.check(token)) {
-      result.push(escapeText(token.toString()))
-      continue
+      if (token.t === 'nl' && opts.nl2br) {
+        result.push('<br>\n')
+        continue
+      } else if (!token.isLink || !opts.check(token)) {
+        result.push(escapeText(token.toString()))
+        continue
+      }
+
+      const {
+        formatted,
+        formattedHref,
+        tagName
+      } = opts.resolve(token)
+
+      const link = [`<${tagName} href="/open?url=${escapeAttr(formattedHref)}"`]
+
+      link.push('target="_blank"')
+      link.push('rel="nofollow noopener"')
+
+      link.push(`>${escapeText(formatted)}</${tagName}>`)
+      result.push(link.join(''))
     }
 
-    const {
-      formatted,
-      formattedHref,
-      tagName
-    } = opts.resolve(token)
-
-    const link = [`<${tagName} href="/open?url=${escapeAttr(formattedHref)}"`]
-
-    link.push('target="_blank"')
-    link.push('rel="nofollow noopener"')
-
-    link.push(`>${escapeText(formatted)}</${tagName}>`)
-    result.push(link.join(''))
+    const resultReally = result.join('')
+    el.innerHTML = resultReally
   }
-
-  const resultReally = result.join('')
-  el.innerHTML = resultReally
 }
 
-Vue.directive('linkified', install)
+Vue.directive('linkified', object)
