aecee7a7622019138562a3a12b0fedb3fdfca0df
Katelinlis <56870191+katelinlis@users.noreply.github.com>
2022-02-19 20:28:29 +0300

    Create be.js

---
+++ lang/be.js
@@ -0,0 +1,25 @@
+export default {
+  MyPage: 'Моя страница',
+  Contacts: 'Контакты',
+  Feed: 'Лента',
+  UsersCatalog: 'Список пользователей',
+  AboutProject: 'О проекте',
+  Wall: 'Стена',
+  Send: 'Отправить',
+  AnswerPost: 'Ответить',
+  LikePost: 'лайк',
+  FriendsUser: 'Ваша сеть',
+  Login: 'Войти',
+  Logout: 'Выйти',
+  InputUsername: 'Введите ваш логин',
+  InputPassword: 'Введите ваш пароль',
+  UserName: 'Имя пользователя',
+  UserPassword: 'Пароль',
+  SavePassword: 'Запомнить пароль',
+  Welcome: 'Добро пожаловать',
+  YouDontHaveAccount: 'У Вас нет аккаунта',
+  Reigster: 'Регистрация',
+  InFreeDomSocialNetwork: 'в свободную социальную сеть',
+  WeAreOnlyOneWeAreOnlyOneTogether: 'Мы всего лишь одни, вместе мы одно!'
+
+}
