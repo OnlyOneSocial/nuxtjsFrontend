348c73358d1cde79e62031d65538e1b159984cf0
Zeik <36483168+SuperiorZeik@users.noreply.github.com>
2022-03-04 00:12:44 +0800

    rename zh to zh_cn and translate

---
+++ lang/zh_cn.js
@@ -0,0 +1,33 @@
+export default { 
+  MyPage: '我的页面',
+  Contacts: '我的联络人',
+  Im: '对话',
+  Feed: '动态',
+  UsersCatalog: '使用者目录',
+  AboutProject: '关于',
+  // page User
+  Friends: '朋友',
+  Subscribes: '订阅',
+  Requests: '请求',
+  BirthDay: '生日',
+  Gender: '性别',
+  Location: '位置',
+  ShortDesc: '简述',
+  // end page User
+  Send: '传送',
+  AnswerPost: '回答',
+  LikePost: '赞',
+  FriendsUser: '朋友',
+  Login: '登入',
+  Logout: '登出',
+  UserName: '使用者名称',
+  UserPassword: '密码',
+  InputUsername: '输入你的使用者名称',
+  InputPassword: '输入你的密码',
+  SavePassword: '储存密码',
+  Welcome: '欢迎',
+  YouDontHaveAccount: '你尚未拥有一个账号',
+  Reigster: '注册',
+  InFreeDomSocialNetwork: '在自由的社交网络',
+  WeAreOnlyOneWeAreOnlyOneTogether: 'We are only one, We are only one together'
+}
